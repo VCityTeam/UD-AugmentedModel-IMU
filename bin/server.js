3a95ca942808e07cfcce37c0a476619bf3dc866d
CorentinLIRIS <corentin.chignin@hotmail.fr>
2024-07-10 17:19:57 +0200

    First GET and POST app

--- bin/server.js
+++ bin/server.js
@@ -1,8 +1,24 @@
 // Code adapted from https://www.npmjs.com/package/express
 const path = require('path');
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+let date = null;
+
+app.post('/', (req, res) => {
+    console.log(req.body.date);
+    date = req.body.date;
+    res.send(date);
+})
+
+app.get('/', (req, res) => {
+    // res.send();
+    res.send(date);
+})
 
 app.use(express.static(path.resolve(__dirname, '../')));
 
