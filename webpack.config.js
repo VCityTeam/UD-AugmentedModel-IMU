ac63ca30a9ef1da8b0c0f5b4662333e4d8be016e
Mathieu Livebardon <mathieu.livebardon@universite-lyon.fr>
2024-07-11 11:33:34 +0200

    refacto(monorepo):add package.json at the root and add env variable to configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+const DIR_APP = process.env.DIR_APP;
+
+module.exports = {
+  entry: `./${DIR_APP}/src/index.js`,
+  output: {
+    filename: "bundle.js",
+    publicPath: `./${DIR_APP}/public`,
+    path: path.resolve(__dirname, `./${DIR_APP}/public/dist`),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  mode: "development",
+  devtool: "source-map",
+};
